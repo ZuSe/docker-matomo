eb0914237cad09e90c29434d5318f6eb8b33c8bf
Patrick Palacin <patrick@palacin.de>
2026-02-05 13:30:33 +0100

    feat: migrate PHP configuration from version 8.3 to 8.5 and update ownership scripts

--- rootfs/etc/cont-init.d/02-fix-perms.sh
+++ rootfs/etc/cont-init.d/02-fix-perms.sh
@@ -10,17 +10,15 @@ mkdir -p /data/config \
   /data/tmp \
   /var/lib/nginx \
   /var/log/nginx \
-  /var/log/php83 \
+  /var/log/php85 \
   /var/run/nginx \
   /var/run/php-fpm
+
+# Recursively fix ownership on /data to handle files from previous pod runs
+echo "Fixing /data ownership recursively..."
+chown -R matomo:matomo /data
+
 chown matomo:matomo \
-  /data \
-  /data/config \
-  /data/geoip \
-  /data/misc \
-  /data/plugins \
-  /data/session \
-  /data/tmp \
   /var/www/matomo/plugins \
   /var/www/matomo/matomo.js \
   /var/www/matomo/piwik.js \
@@ -29,7 +27,7 @@ chown -R matomo:matomo \
   /tpls \
   /var/lib/nginx \
   /var/log/nginx \
-  /var/log/php83 \
+  /var/log/php85 \
   /var/run/nginx \
   /var/run/php-fpm \
   /var/www/matomo/config \
